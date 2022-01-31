28cb200f02a4a99c726f8818a9d38bb173463dd2
Samisai <waliul.sami.ahmed@gmail,com>
2022-01-30 23:41:38 -0700

    Small example changes

--- quiz.py
+++ quiz.py
@@ -2,7 +2,7 @@ from unitCircle import unitCircle
 
 circle = unitCircle()
 
-quiz = circle.generateQuiz(10, "degtorad")
+quiz = circle.generateQuiz(10, "degradtocoor")
 print(quiz[0]) # quiz
-print()
+print("\n\n\n")
 print(quiz[1]) # key
