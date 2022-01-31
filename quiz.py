c6a28d4cbe794438a23cacc52bdd66bea2e166de
Samisai <waliul.sami.ahmed@gmail,com>
2022-01-30 23:53:50 -0700

    add more to quiz.py
    add other version of quiz commented out

--- quiz.py
+++ quiz.py
@@ -3,6 +3,7 @@ from unitCircle import unitCircle
 circle = unitCircle()
 
 quiz = circle.generateQuiz(10, "degradtocoor")
+# quiz = circle.generateQuiz(10, "degtorad")
 print(quiz[0]) # quiz
 print("\n\n\n")
 print(quiz[1]) # key
